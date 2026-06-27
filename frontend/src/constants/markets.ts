b7327e29d6036d58f81f05be0ccefdec18853ed1
dcast-gif <dylancastell@gmail.com>
2026-06-27 23:32:22 +0100

    Create markets.ts

---
+++ frontend/src/constants/markets.ts
@@ -0,0 +1,42 @@
+export const MARKETS = [
+  {
+    id: "match_winner",
+    name: "Match Winner",
+  },
+  {
+    id: "over_under",
+    name: "Over / Under",
+  },
+  {
+    id: "both_teams_to_score",
+    name: "Both Teams To Score",
+  },
+  {
+    id: "double_chance",
+    name: "Double Chance",
+  },
+  {
+    id: "draw_no_bet",
+    name: "Draw No Bet",
+  },
+  {
+    id: "player_goals",
+    name: "Player Goals",
+  },
+  {
+    id: "corners",
+    name: "Corners",
+  },
+  {
+    id: "cards",
+    name: "Cards",
+  },
+  {
+    id: "shots",
+    name: "Shots",
+  },
+  {
+    id: "custom",
+    name: "Custom",
+  },
+];
