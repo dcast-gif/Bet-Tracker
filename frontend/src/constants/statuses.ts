bcfc756a701fca26c929121b84399e0092448d96
dcast-gif <dylancastell@gmail.com>
2026-06-27 23:32:56 +0100

    Create statuses.ts

---
+++ frontend/src/constants/statuses.ts
@@ -0,0 +1,33 @@
+export const BET_STATUSES = [
+  {
+    id: "not_started",
+    name: "Not Started",
+  },
+  {
+    id: "live",
+    name: "Live",
+  },
+  {
+    id: "won",
+    name: "Won",
+  },
+  {
+    id: "lost",
+    name: "Lost",
+  },
+];
+
+export const EVENT_STATUSES = [
+  {
+    id: "scheduled",
+    name: "Scheduled",
+  },
+  {
+    id: "live",
+    name: "Live",
+  },
+  {
+    id: "finished",
+    name: "Finished",
+  },
+];
