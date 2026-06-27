ce8209e5883bc811caaefd9720e81fd026375d90
dcast-gif <dylancastell@gmail.com>
2026-06-27 23:26:20 +0100

    Create sports.ts

---
+++ frontend/src/constants/sports.ts
@@ -0,0 +1,32 @@
+export const SPORTS = [
+  {
+    id: "football",
+    name: "Football",
+    emoji: "⚽",
+  },
+  {
+    id: "tennis",
+    name: "Tennis",
+    emoji: "🎾",
+  },
+  {
+    id: "basketball",
+    name: "Basketball",
+    emoji: "🏀",
+  },
+  {
+    id: "cricket",
+    name: "Cricket",
+    emoji: "🏏",
+  },
+  {
+    id: "rugby",
+    name: "Rugby",
+    emoji: "🏉",
+  },
+  {
+    id: "horse_racing",
+    name: "Horse Racing",
+    emoji: "🏇",
+  },
+];
