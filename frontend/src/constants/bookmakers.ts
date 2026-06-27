cf82049bc229c835bb18b74c580473713ac60333
dcast-gif <dylancastell@gmail.com>
2026-06-27 23:31:57 +0100

    Create bookmakers.ts

---
+++ frontend/src/constants/bookmakers.ts
@@ -0,0 +1,38 @@
+export const BOOKMAKERS = [
+  {
+    id: "bet365",
+    name: "Bet365",
+  },
+  {
+    id: "paddypower",
+    name: "Paddy Power",
+  },
+  {
+    id: "skybet",
+    name: "Sky Bet",
+  },
+  {
+    id: "williamhill",
+    name: "William Hill",
+  },
+  {
+    id: "betfair",
+    name: "Betfair",
+  },
+  {
+    id: "ladbrokes",
+    name: "Ladbrokes",
+  },
+  {
+    id: "coral",
+    name: "Coral",
+  },
+  {
+    id: "fanduel",
+    name: "FanDuel",
+  },
+  {
+    id: "draftkings",
+    name: "DraftKings",
+  },
+];
