fa3ed8e8343a8e8f69add359a4f62974ad31678b
dcast-gif <dylancastell@gmail.com>
2026-06-28 16:18:12 +0100

    Update uploadScreenshot.ts

--- frontend/src/services/uploadScreenshot.ts
+++ frontend/src/services/uploadScreenshot.ts
@@ -1,20 +1,5 @@
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import { storage } from "./firebaseClient";
+import { uploadToSupabaseStorage } from "./supabaseStorage";
 
 export async function uploadScreenshot(file: File): Promise<string> {
-  const fileName = `${Date.now()}-${file.name}`;
-
-  const storageRef = ref(storage, `bet-slips/${fileName}`);
-
-  const uploadTask = uploadBytes(storageRef, file);
-
-  const timeout = new Promise<never>((_, reject) => {
-    setTimeout(() => {
-      reject(new Error("Upload timed out"));
-    }, 20000);
-  });
-
-  await Promise.race([uploadTask, timeout]);
-
-  return getDownloadURL(storageRef);
+  return uploadToSupabaseStorage(file);
 }
