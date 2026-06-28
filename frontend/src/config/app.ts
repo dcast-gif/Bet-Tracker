b4af961997f2a2e7960ac5cefa82dcc0fee00ce2
dcast-gif <dylancastell@gmail.com>
2026-06-28 03:42:41 +0100

    Create app.ts

---
+++ frontend/src/config/app.ts
@@ -0,0 +1,34 @@
+export const APP_CONFIG = {
+  name: "Bet Tracker",
+
+  version: "0.1.0",
+
+  supportedSports: [
+    "football",
+    "tennis",
+    "basketball",
+    "cricket",
+    "rugby",
+    "horse_racing",
+  ],
+
+  supportedBookmakers: [
+    "bet365",
+    "paddypower",
+    "skybet",
+    "williamhill",
+    "betfair",
+    "ladbrokes",
+    "coral",
+    "fanduel",
+    "draftkings",
+  ],
+
+  maxImageSizeMb: 10,
+
+  acceptedImageTypes: [
+    "image/jpeg",
+    "image/png",
+    "image/webp",
+  ],
+};
